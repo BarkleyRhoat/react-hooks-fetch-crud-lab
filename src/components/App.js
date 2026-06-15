0eba9e15161d08ed74f710bf45ba81859c8a7d83
BarkleyRhoat <barkleyrhoat@gmail.com>
2026-06-15 12:12:55 -0400

    GET/questions diplaying

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
+const questionApi = "http://localhost:4000/questions";
+
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([])
+
+  useEffect(() => {
+    fetch(questionApi)
+      .then((response) => response.json())
+      .then((data) => setQuestions(data))
+  }, [])
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm /> : <QuestionList questions={questions} />}
     </main>
   );
 }
